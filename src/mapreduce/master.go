9da6d3c26e92178963147dd132147e7553ddb4ab
dimitar9 <dimitar9909@gmail.com>
2014-05-18 23:56:53 -0400

    initial try for master.go

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -28,5 +28,6 @@ func (mr *MapReduce) KillWorkers() *list.List {
 
 func (mr *MapReduce) RunMaster() *list.List {
   // Your code here
+  RunWorker("127.0.0.1", "worker1", DoMap, DoReduce, 5)
   return mr.KillWorkers()
 }
